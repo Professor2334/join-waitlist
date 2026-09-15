9c20bec5d4bc3e81d4eca4c835f6b211b510365e
Kanyinsola Sanni <kanyinsolasanni12@gmail.com>
2026-09-15 16:11:59 +0100

    committt

--- src/components/hero/HeroVisual.tsx
+++ src/components/hero/HeroVisual.tsx
@@ -5,7 +5,7 @@ export default function HeroVisual() {
   return (
     <div className="relative w-full h-[400px] md:h-[500px] lg:h-[650px] max-w-[650px] mx-auto lg:ml-auto lg:mr-0">
       <Image
-        src="/svg image.svg"
+        src="/hero-image.png"
         alt="SMART.INC Hero Visual"
         fill
         priority
